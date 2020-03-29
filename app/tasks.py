27b8b4cbfe85448e8fedaf0fb19c322fe310ccf6
sqr <artubr@gmail.com>
2020-03-29 22:51:22 +0200

    deleted -re, added GOP 50frames debug for dailymotion chopiness

--- app/tasks.py
+++ app/tasks.py
@@ -21,12 +21,12 @@ def restream(origin, server, stream_key):
     stream_server = generate_url(server, stream_key)
     try:
         stream_map = None
-        stream1 = ffmpeg.input(get_manifest(origin), re=None)
+        stream1 = ffmpeg.input(get_manifest(origin))
         stream2 = ffmpeg.input('mosca_66.png')
         stream_ol = ffmpeg.overlay(stream1, stream2, x='main_w-overlay_w-50', y='50')
         stream_ol = ffmpeg.filter(stream_ol, 'fps', fps=25, round='up')
         a1 = stream1.audio
-        stream = ffmpeg.output(stream_ol, a1, stream_server, format='flv', vcodec='libx264', s='1280x720', acodec='aac', preset='veryfast', g='120', crf='23', maxrate='4M', bufsize='5M', channel_layout='stereo')
+        stream = ffmpeg.output(stream_ol, a1, stream_server, format='flv', vcodec='libx264', s='1280x720', acodec='aac', preset='veryfast', g='50', crf='23', maxrate='4M', bufsize='5M', channel_layout='stereo')
         print(stream.get_args())
         ffmpeg.run(stream)
         set_complete()
