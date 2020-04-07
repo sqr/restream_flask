41848cb045972b583da268dac810b8604ae3cca1
sqr <artubr@gmail.com>
2020-04-07 20:23:08 +0200

    threads count increased to 2

--- app/tasks.py
+++ app/tasks.py
@@ -40,9 +40,9 @@ def restream(origin, server, stream_key):
         else:
             stream1_audio = stream1['1']
         if 'dailymotion' in server:
-            stream = ffmpeg.output(stream_ol, stream1_audio, stream_server, format='flv', vcodec='libx264', acodec='aac', preset='veryfast', g='50', threads='1', crf='23', maxrate='4M', bufsize='5M', channel_layout='stereo')
+            stream = ffmpeg.output(stream_ol, stream1_audio, stream_server, format='flv', vcodec='libx264', acodec='aac', preset='veryfast', g='50', threads='2', crf='23', maxrate='4M', bufsize='5M', channel_layout='stereo')
         else:
-            stream = ffmpeg.output(stream_ol, stream1_audio, stream_server, format='flv', vcodec='libx264', acodec='aac', preset='veryfast', g='50', threads='1', s='1280x720', crf='23', maxrate='4M', bufsize='5M', channel_layout='stereo')
+            stream = ffmpeg.output(stream_ol, stream1_audio, stream_server, format='flv', vcodec='libx264', acodec='aac', preset='veryfast', g='50', threads='2', s='1280x720', crf='23', maxrate='4M', bufsize='5M', channel_layout='stereo')
         ffmpeg.run(stream)
         set_complete()
     except:
