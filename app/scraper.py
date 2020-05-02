2c35195d63e527051fd6075e9b1f5b0714a011b3
Arturo Bracero <artubr@gmail.com>
2020-05-02 22:58:28 +0200

    scraper implementation

--- app/scraper.py
+++ app/scraper.py
@@ -5,7 +5,7 @@ import re
 URL_PRESIDENTE = 'https://www.lamoncloa.gob.es/multimedia/videos/presidente/Paginas/2020/directo.aspx'
 URL_MINISTROS = 'https://www.lamoncloa.gob.es/multimedia/videos/consejoministros/Paginas/directo.aspx'
 
-def get_origin(URL) {
+def get_origin(URL):
     r = requests.get(URL)
     soup = BeautifulSoup(r.content, 'html5lib') 
     soup_div = soup.find("div", {"id": "div_video_contenedor"})
@@ -14,4 +14,3 @@ def get_origin(URL) {
 
     urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', ss)
     return (urls[0].translate({ord(i): None for i in ',\''}))
-}
