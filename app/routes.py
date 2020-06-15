0fba30d7fa42b4ed7e119ce07e49b381cf03912b
sqr <artubr@gmail.com>
2020-06-15 21:57:49 +0200

    fixing subprocess run

--- app/routes.py
+++ app/routes.py
@@ -212,8 +212,8 @@ def marianizer():
     if form.submit.data and form.validate():
         videotitle = form.title.data
         tweeturl = form.tweet.data
-        # videoname = "-".join([tweeturl.split("/")[-1], "1"]) + ".mp4"
-        # subprocess.run(['download-twitter-resources', '-c', 'twitter_secrets.json', '--video', '--tweet', tweeturl, 'video'], shell=True)
+        videoname = "-".join([tweeturl.split("/")[-1], "1"]) + ".mp4"
+        subprocess.run(['download-twitter-resources', '-c', 'twitter_secrets.json', '--video', '--tweet', tweeturl, ' video'], shell=True)
         # subprocess.call(['python', 'mp42youtube.py', '--file', videoname, '--title', videotitle], shell=True)
         # file1 = open('id.txt', 'r')
         # video = ('https://www.youtube.com/watch?v=' + file1.read())
